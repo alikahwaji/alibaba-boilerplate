c768ffcdeaa1a8dbd307438a57e5a68a836ca7f2
Ali Kahwaji <ali.kahwaji@hotmail.com>
2017-10-12 20:20:05 +0400

    Adding some JSX code

--- thingsbook/public/scripts/app.js
+++ thingsbook/public/scripts/app.js
@@ -5,20 +5,53 @@ console.log('App.js is running');
 //JSX - JavaScript XML
 
 var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
+    'div',
     null,
-    'Things Book App'
-  ),
-  ' ',
-  React.createElement(
-    'p',
+    React.createElement(
+        'h1',
+        null,
+        'Things Book App developed by Ali Kahwaji'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'item two'
+        )
+    )
+);
+
+var templateTwo = React.createElement(
+    'div',
     null,
-    'This is some info'
-  )
+    React.createElement(
+        'h1',
+        null,
+        'Ali Kahwaji'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 37'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Auckland'
+    )
 );
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- thingsbook/src/app.js
+++ thingsbook/src/app.js
@@ -2,7 +2,24 @@ console.log('App.js is running')
 
 //JSX - JavaScript XML
 
-var template = <div><h1>Things Book App</h1> <p>This is some info</p></div>
+var template = (
+<div>
+    <h1>Things Book App developed by Ali Kahwaji</h1> 
+    <p>This is some info</p>
+    <ol>
+        <li>item one</li>
+        <li>item two</li>
+    </ol>
+</div>
+)
+
+var templateTwo = (
+    <div>
+        <h1>Ali Kahwaji</h1>
+        <p>Age: 37</p>
+        <p>Location: Auckland</p>
+    </div>
+)
 var appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(templateTwo, appRoot)
