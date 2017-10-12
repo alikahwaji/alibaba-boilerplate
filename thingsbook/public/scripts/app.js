371cce6fdf594b088b494fa48ea10750947ff3ad
Ali Kahwaji <ali.kahwaji@hotmail.com>
2017-10-12 19:10:41 +0400

    Setting up babel

--- thingsbook/public/scripts/app.js
+++ thingsbook/public/scripts/app.js
@@ -1,14 +1,14 @@
-console.log('App.js is running')
+'use strict';
+
+console.log('App.js is running');
 
 //JSX - JavaScript XML
 
-//var template = <p>This is JSX from app.js</p>
 var template = React.createElement(
-    "h1",
-    { id: "someid" },
-    "This is JSX from app.js"
-  );
-  
-var appRoot = document.getElementById('app')
+  'h1',
+  null,
+  'Things Book App'
+);
+var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(template, appRoot);
---
+++ thingsbook/src/app.js
@@ -0,0 +1,8 @@
+console.log('App.js is running')
+
+//JSX - JavaScript XML
+
+var template = <h1>Things Book App</h1>  
+var appRoot = document.getElementById('app')
+
+ReactDOM.render(template, appRoot)
