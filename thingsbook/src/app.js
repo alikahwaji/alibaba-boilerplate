fce89eb8a418a50c162ce17447a295c53ddea0fc
Ali Kahwaji <ali.kahwaji@hotmail.com>
2017-10-17 16:23:52 +0400

    Moving counter.js to jsx folder

--- thingsbook/src/app.js
+++ thingsbook/src/app.js
@@ -19,36 +19,4 @@ const template = (
     </ol>
 </div>
 )
-
-let count =0
-const addOne = () => {
-    count++
-    renderCounterApp()
-}
-const minusOne = () => {
-    count--
-    renderCounterApp()
-}
-
-const reset = () => {
-    count = 0
-    renderCounterApp()
-}
-
 const appRoot = document.getElementById('app')
-
-const renderCounterApp = () => {
-    const templateTwo = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>Reset button</button>
-        </div>
-
-    )
-
-    ReactDOM.render(templateTwo, appRoot)
-}
-
-renderCounterApp()
