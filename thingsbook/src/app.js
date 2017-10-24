f6b6e2229d5c5c1891f22583601b5fb7636dcff8
Ali Kahwaji <ali.kahwaji@hotmail.com>
2017-10-24 14:57:38 +1300

    Adding events handlers to the components

--- thingsbook/src/app.js
+++ thingsbook/src/app.js
@@ -26,22 +26,30 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+    handlePick() {
+        alert('handlePick')
+    }
     render() {
         return (
             <div>
-                <button>What should I do </button>
+                <button onClick={this.handlePick}>What should I do </button>
             </div>
         )
     }
 }
 
 class Options extends React.Component {
+    handleRemoveAll() {
+        alert('Remove Option')
+    }
     render() {
         return(
             <div>
+                <button onClick={this.handleRemoveAll}>Remove All</button>
                 {
                     this.props.options.map((option) => <Option key={option} optionText={option}/>)
                 }
+                
             </div>
         )
     }
@@ -58,10 +66,22 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+    handleAddOption(e) {
+        e.preventDefault()
+
+        const option = e.target.elements.option.value.trim()
+
+        if (option) {
+            alert(option)
+        }
+    }
     render() {
         return (
             <div>
-                <h2>Add button here</h2>
+                <form onSubmit={this.handleAddOption}>
+                    <input type='text' name='option' />
+                    <button>Add Option</button>
+                </form>
             </div>
         )
     }
