d6a2ef0636b57ae67c5f281e9b7937189a3747ed
Ali Kahwaji <ali.kahwaji@hotmail.com>
2017-10-26 10:06:49 +1300

    Converting Header class componentto stateless component

--- thingsbook/public/scripts/app.js
+++ thingsbook/public/scripts/app.js
@@ -83,37 +83,33 @@ var ThingsToDoApp = function (_React$Component) {
     return ThingsToDoApp;
 }(React.Component);
 
-var Header = function (_React$Component2) {
-    _inherits(Header, _React$Component2);
+var Header = function Header(props) {
+    return React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            props.title
+        ),
+        React.createElement(
+            'h2',
+            null,
+            props.subtitle
+        )
+    );
+};
 
-    function Header() {
-        _classCallCheck(this, Header);
-
-        return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
-    }
-
-    _createClass(Header, [{
-        key: 'render',
-        value: function render() {
-            return React.createElement(
-                'div',
-                null,
-                React.createElement(
-                    'h1',
-                    null,
-                    this.props.title
-                ),
-                React.createElement(
-                    'h2',
-                    null,
-                    this.props.subtitle
-                )
-            );
-        }
-    }]);
-
-    return Header;
-}(React.Component);
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>{this.props.title}</h1>
+//                 <h2>{this.props.subtitle}</h2>
+//             </div>
+//         )
+//     }
+// }
 
 var Action = function Action(props) {
     return React.createElement(
@@ -145,8 +141,8 @@ var Action = function Action(props) {
 //     }
 // }
 
-var Options = function (_React$Component3) {
-    _inherits(Options, _React$Component3);
+var Options = function (_React$Component2) {
+    _inherits(Options, _React$Component2);
 
     function Options() {
         _classCallCheck(this, Options);
@@ -175,8 +171,8 @@ var Options = function (_React$Component3) {
     return Options;
 }(React.Component);
 
-var Option = function (_React$Component4) {
-    _inherits(Option, _React$Component4);
+var Option = function (_React$Component3) {
+    _inherits(Option, _React$Component3);
 
     function Option() {
         _classCallCheck(this, Option);
@@ -199,19 +195,19 @@ var Option = function (_React$Component4) {
     return Option;
 }(React.Component);
 
-var AddOption = function (_React$Component5) {
-    _inherits(AddOption, _React$Component5);
+var AddOption = function (_React$Component4) {
+    _inherits(AddOption, _React$Component4);
 
     function AddOption(props) {
         _classCallCheck(this, AddOption);
 
-        var _this5 = _possibleConstructorReturn(this, (AddOption.__proto__ || Object.getPrototypeOf(AddOption)).call(this, props));
+        var _this4 = _possibleConstructorReturn(this, (AddOption.__proto__ || Object.getPrototypeOf(AddOption)).call(this, props));
 
-        _this5.handleAddOption = _this5.handleAddOption.bind(_this5);
-        _this5.state = {
+        _this4.handleAddOption = _this4.handleAddOption.bind(_this4);
+        _this4.state = {
             error: undefined
         };
-        return _this5;
+        return _this4;
     }
 
     _createClass(AddOption, [{
--- thingsbook/src/app.js
+++ thingsbook/src/app.js
@@ -57,16 +57,26 @@ class ThingsToDoApp extends React.Component {
     }
 }
 
-class Header extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>{this.props.title}</h1>
-                <h2>{this.props.subtitle}</h2>
-            </div>
-        )
-    }
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <h2>{props.subtitle}</h2>
+        </div>
+    )
+
 }
+
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <h1>{this.props.title}</h1>
+//                 <h2>{this.props.subtitle}</h2>
+//             </div>
+//         )
+//     }
+// }
 
 const Action = (props) => {
     return (
