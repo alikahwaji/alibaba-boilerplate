2d794abd51140039606814867e8f1a79e109de3c
Ali Kahwaji <ali.kahwaji@hotmail.com>
2017-10-25 14:36:06 +1300

    adding set state to the buttons counts

--- thingsbook/src/JSX/counter.js
+++ thingsbook/src/JSX/counter.js
@@ -5,20 +5,35 @@ class Counter extends React.Component {
         this.handleAddOne = this.handleAddOne.bind(this)
         this.handleMinusOne = this.handleMinusOne.bind(this)
         this.handleReset = this.handleReset.bind(this)
+        this.state = {
+            count: 0
+        }
     }
     handleAddOne() {
-        console.log('handleAddOne')
+        this.setState((prevState) => {
+            return {
+                count: prevState.count + 1
+            }
+        })
     }
     handleMinusOne() {
-        console.log('handleMinusOne')
+        this.setState((prevState) => {
+            return{
+                count: prevState.count -1
+            }
+        })
     }
     handleReset() {
-        console.log('handleReset')
+        this.setState((prevState) => {
+            return {
+                count: 0
+            }
+        })
     }
     render() {
         return (
             <div>
-                <h1>Count: </h1>
+                <h1>Count: {this.state.count}</h1>
                 <button onClick={this.handleAddOne}>+1</button>
                 <button onClick={this.handleMinusOne}>-1</button>
                 <button onClick={this.handleReset}>reset</button>
