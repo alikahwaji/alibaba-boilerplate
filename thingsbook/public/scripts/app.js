8ea27abd1264ea72a1982e1fefa2a73fe70064d4
Ali Kahwaji <ali.kahwaji@hotmail.com>
2017-10-27 10:10:23 +1300

    Adding lifecycle method to counter.js

--- thingsbook/public/scripts/app.js
+++ thingsbook/public/scripts/app.js
@@ -26,6 +26,25 @@ var Counter = function (_React$Component) {
     }
 
     _createClass(Counter, [{
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            var stringCount = localStorage.getItem('count');
+            var count = parseInt(stringCount, 10);
+
+            if (!isNaN(count)) {
+                this.setState(function () {
+                    return { count: count };
+                });
+            }
+        }
+    }, {
+        key: 'componentDidUpdate',
+        value: function componentDidUpdate(prevProps, prevState) {
+            if (prevState.count !== this.state.count) {
+                localStorage.setItem('count', this.state.count);
+            }
+        }
+    }, {
         key: 'handleAddOne',
         value: function handleAddOne() {
             this.setState(function (prevState) {
