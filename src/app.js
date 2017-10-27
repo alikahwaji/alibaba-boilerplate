c85c8d9f6485ee17e4df4dc808fd3fc423e8caba
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-10-27 22:45:46 +1300

    Installing validator package

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+import validator from 'validator'
+
+console.log(validator.isEmail('test@gmail.com'))
+
 // import './utils'
 // import sub, {square, add} from './utils'
 
@@ -6,8 +10,8 @@
 // console.log(add(100, 23))
 // console.log(sub(100, 81))
 
-import isSenior, {isAdult, canDrink} from './utils'
+// import isSenior, {isAdult, canDrink} from './utils'
 
-console.log(isAdult(10))
-console.log(canDrink(28))
-console.log(isSenior(63))
+// console.log(isAdult(10))
+// console.log(canDrink(28))
+// console.log(isSenior(63))
