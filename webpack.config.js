e2b405e56985607883c173a2755623a2993ad0ca
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-10-27 15:54:44 +1300

    Debugging the public and src folder and putting in the project root

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 const path = require('path')
-=======
 
-console.log(__dirname)
->>>>>>> 8cce74047ab9faedb5a47a09c67cbcb7be697e7a
-
-module.exports ={
-    entry: './src/app.js',
-    output: {
-<<<<<<< HEAD
-        path: path.join(__dirname, 'public'),
-=======
-        path:'',
->>>>>>> 8cce74047ab9faedb5a47a09c67cbcb7be697e7a
-        filename: 'bundle.js'
-    }
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  }
 }
