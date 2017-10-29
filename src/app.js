52de64a84bbdecc30746b4e31680bb1cc43969af
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-10-29 14:21:41 +1300

    Modified app.js and ThingsToDo component

--- src/app.js
+++ src/app.js
@@ -3,29 +3,3 @@ import ReactDOM from 'react-dom'
 import ThingsToDoApp from './components/ThingsToDo'
 
 ReactDOM.render(<ThingsToDoApp />, document.getElementById('app'))
-
-class OldSyntax {
-  constructor () {
-    this.name = 'Mike'
-  }
-  getGreeting() {
-    return`Hi. my name is ${this.name}.`
-  }
-}
-
-const oldSyntax = new OldSyntax()
-console.log(oldSyntax.getGreeting())
-
-// ---------
-
-class NewSyntax {
-  name = 'Max'
-  getGreeting = () => {
-    return`Hi. my name is ${this.name}.`
-  }
-
-}
-
-const newSyntax = new NewSyntax()
-const newGetGreeting = newSyntax.getGreeting
-console.log(newGetGreeting())
--- src/components/ThingsToDo.js
+++ src/components/ThingsToDo.js
@@ -5,16 +5,35 @@ import Action from './Action'
 import Header from './Header'
 
 class ThingsToDoApp extends React.Component {
-  constructor (props) {
-    super(props)
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this)
-    this.handlePick = this.handlePick.bind(this)
-    this.handleAddOption = this.handleAddOption.bind(this)
-    this.handleDeleteOption = this.handleDeleteOption.bind(this)
-    this.state = {
-      options: props.options
-    }
+  state = {
+    options: []
   }
+
+  handleDeleteOptions = () => {
+    this.setState(() => ({ options: [] }))
+  }
+
+  handleDeleteOption = (optionToRemove) => {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => optionToRemove !== option)
+    }))
+  }
+
+  handlePick = () => {
+    const randomNum = Math.floor(Math.random() * this.state.options.length)
+    const option = this.state.options[randomNum]
+    alert(option)
+  }
+  handleAddOption = (option) => {
+    if (!option) {
+      return 'Enter valid value to add item'
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists'
+    }
+    this.setState((prevState) => ({options: prevState.options.concat(option)}))
+  }
+
+
   componentDidMount () {
     try {
       const json = localStorage.getItem('options')
@@ -37,29 +56,7 @@ class ThingsToDoApp extends React.Component {
   componentWillUnmount () {
     console.log('componentWillUnmount')
   }
-  handleDeleteOptions () {
-    this.setState(() => ({ options: [] }))
-  }
-
-  handleDeleteOption (optionToRemove) {
-    this.setState((prevState) => ({
-      options: prevState.options.filter((option) => optionToRemove !== option)
-    }))
-  }
-
-  handlePick () {
-    const randomNum = Math.floor(Math.random() * this.state.options.length)
-    const option = this.state.options[randomNum]
-    alert(option)
-  }
-  handleAddOption (option) {
-    if (!option) {
-      return 'Enter valid value to add item'
-    } else if (this.state.options.indexOf(option) > -1) {
-      return 'This option already exists'
-    }
-    this.setState((prevState) => ({options: prevState.options.concat(option)}))
-  }
+  
   render () {
     const subtitle = 'Put your plans in the app'
     return (
