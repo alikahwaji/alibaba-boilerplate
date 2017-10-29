8244b48ee9cdf10932a71f920e1bdb6d13ee323b
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-10-30 09:32:58 +1300

    Installing react-modal

--- src/app.js
+++ src/app.js
@@ -2,21 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ThingsToDoApp from './components/ThingsToDo'
 
-const Layout = (props) => {
-  return (
-    <div>
-      <p>header</p>
-      {props.children}
-      <p>footer</p>
-    </div>
-  )
-}
-
-ReactDOM.render((
-  <Layout>
-    <div>
-      <h1>Page title</h1>
-      <p>This my Page</p>
-    </div>
-  </Layout>
-), document.getElementById('app'))
+ReactDOM.render(<ThingsToDoApp />, document.getElementById('app'))
