86ed3cd919886a4aed3fbfadd09782bab73b7613
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-11-02 10:35:51 +1300

    installing css packages for webpack config

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import ThingsToDoApp from './components/ThingsToDo'
+import './styles/styles.css'
 
 ReactDOM.render(<ThingsToDoApp />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,13 @@ module.exports = {
     rules: [{
       loader: 'babel-loader',
       test: /\.js$/,
-      exclude:/node_modules/
+      exclude: /node_modules/
+    }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
     }]
   },
   devtool: 'cheap-module-eval-source-map',
