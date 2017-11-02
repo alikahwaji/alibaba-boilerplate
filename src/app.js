9957113c09d84915b4e51fb6f881b4bd87980d3b
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-11-02 14:24:10 +1300

    Loading normalize css package for browser config

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import ThingsToDoApp from './components/ThingsToDo'
+import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 ReactDOM.render(<ThingsToDoApp />, document.getElementById('app'))
