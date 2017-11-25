172d45ae56eda44449f43e0c9e3b0a74d8bf1e35
ali-k-horoeka <ali.kahwaji@hotmail.com>
2017-11-25 16:08:46 +1300

    Using NavLink to call out specific link

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter, Route, Switch, Link} from 'react-router-dom'
+import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
@@ -37,10 +37,10 @@ const NotFoundPage = () => (
 const Header = () => (
   <header>
     <h1>Spend Track</h1>
-    <Link to='/'>Home</Link>
-    <Link to='/create'>Create</Link>
-    <Link to='/edit'>Edit</Link>
-    <Link to='/help'>Help</Link>
+    <NavLink to='/' activeClassName='is-active' exact={true}>Home</NavLink>
+    <NavLink to='/create' activeClassName='is-active'>Create</NavLink>
+    <NavLink to='/edit' activeClassName='is-active'>Edit</NavLink>
+    <NavLink to='/help' activeClassName='is-active'>Help</NavLink>
   </header>
 )
 
